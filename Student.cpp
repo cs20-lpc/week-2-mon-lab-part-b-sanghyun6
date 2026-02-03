90182cfd1f5a0d1cd2fd0ccbeb1ab3d510acbd20
sanghyun6 <sanghyunleeee@gmail.com>
2026-02-02 16:35:58 -0800

    Remove unnecessary comments and clean up code

--- Student.cpp
+++ Student.cpp
@@ -2,37 +2,33 @@
 #include <iostream>
 using namespace std;
 
-// 생성자
 Student::Student()
 {
     cout << "Student object created!" << endl;
 }
 
-// 소멸자
 Student::~Student()
 {
     cout << "Student object destroyed!" << endl;
 }
-
-// 이름 설정
 void Student::setName(string n)
 {
     name = n;
 }
 
-// 나이 설정
+
 void Student::setAge(int a)
 {
     age = a;
 }
 
-// 이름 반환
+
 string Student::getName() const
 {
     return name;
 }
 
-// 나이 반환
+
 int Student::getAge() const
 {
     return age;
